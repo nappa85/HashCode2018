d4c6e3030b9288ec55e4c938113a777f7badc2a7
Marco Napetti <marco.napetti@pixartprinting.com>
2018-01-19 09:30:10 +0100

    Porting finished

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,105 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 struct Pizza {
     rows: usize,
     cols: usize,
-    minIngr: usize,
-    maxCells: usize,
-    matrix: Vec<Vec<String>>
+    min_ingr: usize,
+    max_cells: usize,
+    matrix: Vec<Vec<Option<char>>>,
+    slices: Vec<Slice>,
 }
 
 impl Pizza {
-    fn addRow(&mut self, r: usize, s: String) {
-        self.matrix.push(Vec::with_capacity(self.cols));
-        for (c, v) in s.split(' ').enumerate() {
-            self.matrix[r].push(v.to_owned());
+    pub fn new(rows: usize, cols: usize, min_ingr: usize, max_cells: usize) -> Pizza {
+        Pizza {
+            rows: rows,
+            cols: cols,
+            min_ingr: min_ingr,
+            max_cells: max_cells,
+            matrix: Vec::with_capacity(rows),
+            slices: Vec::new(),
         }
+    }
+
+    pub fn add_row(&mut self, r: usize, s: String) {
+        if self.matrix.len() <= r {
+            self.matrix.push(Vec::with_capacity(self.cols));
+        }
+
+        for c in s.chars() {
+            self.matrix[r].push(Some(c));
+        }
+    }
+
+    pub fn cut(&mut self) -> &Pizza {
+        for row1 in 0..self.rows {
+            for col1 in 0..self.cols {
+                //if it's an already used slot let's skip it
+                if self.matrix[row1][col1] == None {
+                    continue;
+                }
+
+                for row2 in row1..self.rows {
+                    //slices can't overlap
+                    if self.matrix[row2][col1] == None {
+                        break;
+                    }
+
+                    for col2 in col1..self.cols {
+                        //slices can't overlap
+                        if self.matrix[row2][col2] == None {
+                            break;
+                        }
+
+                        if self.is_valid_slice(row1, col1, row2, col2) {
+                            self.save_slice(row1, col1, row2, col2);
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        self
+    }
+
+    fn is_valid_slice(&self, row1: usize, col1: usize, row2: usize, col2: usize) -> bool {
+        if ((row2 - row1) + 1) * ((col2 - col1) + 1) > self.max_cells {
+            return false;
+        }
+
+        let mut ingredients = HashMap::new();
+        for row in row1..row2 + 1 {
+            for col in col1..col2 + 1 {
+                match self.matrix[row][col] {
+                    Some(s) => {
+                        let count = ingredients.entry(s).or_insert(0);
+                        *count += 1;
+                    },
+                    None => return false,
+                }
+            }
+        }
+
+        *ingredients.values().min().unwrap() >= self.min_ingr
+    }
+
+    fn save_slice(&mut self, row1: usize, col1: usize, row2: usize, col2: usize) {
+        for row in row1..row2 {
+            for col in col1..col2 {
+                self.matrix[row][col] = None;
+            }
+        }
+
+        self.slices.push(Slice {
+            row1: row1,
+            col1: col1,
+            row2: row2,
+            col2: col2,
+        });
     }
 }
 
@@ -32,16 +116,33 @@ impl FromStr for Pizza {
             Err(format!("Wrong input {}", d))
         }
         else {
-            let rows:usize = p[0].parse().unwrap();
-            let cols:usize = p[1].parse().unwrap();
-            Ok(Pizza {
-                rows: rows,
-                cols: cols,
-                minIngr: p[2].parse().unwrap(),
-                maxCells: p[3].parse().unwrap(),
-                matrix: Vec::with_capacity(rows),
-            })
+            Ok(Pizza::new(p[0].parse().unwrap(), p[0].parse().unwrap(), p[0].parse().unwrap(), p[0].parse().unwrap()))
         }
+    }
+}
+
+impl ToString for Pizza {
+    fn to_string(&self) -> String {
+        let mut s = self.slices.len().to_string();
+
+        for n in 0..self.slices.len() {
+            s = format!("{}\n{}", s, self.slices[n].to_string());
+        }
+
+        s
+    }
+}
+
+struct Slice {
+    row1: usize,
+    col1: usize,
+    row2: usize,
+    col2: usize,
+}
+
+impl ToString for Slice {
+    fn to_string(&self) -> String {
+        format!("{} {} {} {}", self.row1, self.col1, self.row2, self.col2)
     }
 }
 
@@ -55,6 +156,8 @@ fn main() {
 
     let mut p:Pizza = iter.next().unwrap().unwrap().parse().unwrap();//orribile
     for i in 0..p.rows {
-        p.addRow(i, iter.next().unwrap().unwrap());
+        p.add_row(i, iter.next().unwrap().unwrap());
     }
+
+    println!("{}", p.cut().to_string());
 }
